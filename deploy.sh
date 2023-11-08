dcaddc7a36ee22536ee064b5b7ffc8dfe7d8e134
Tommy Praeger <tommypraeger@gmail.com>
2023-11-07 23:01:34 -0500

    format deploy script and install python dependencies before installing

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,17 @@ if [[ $* != *--skip-react* ]]; then
     aws cloudfront create-invalidation --distribution-id EQ5A8DNTWQ42J --paths "/*"
 fi
 if [[ $* != *--skip-lambda* ]]; then
-cd venv/lib/python3.8/site-packages
-zip -r9 ${OLDPWD}/app.zip . -x "numpy*" "scipy*"
-cd $OLDPWD
-zip -rg app.zip . -x ".git*" "node_modules/*" "venv/*" "*.DS_Store"\
-    ".vscode/*" "deploy.sh" "app/dist/*" "app/static/*" "*__pycache__/*"\
-    "package.json" "package-lock.json" "webpack*" ".babelrc" ".env"\
-    ".eslintrc.js" "README.md"
-aws s3 cp app.zip s3://fantasy-sports-analytics
-rm app.zip
-aws lambda update-function-code --function-name fantasy-sports-analytics\
-    --s3-bucket fantasy-sports-analytics\
-    --s3-key app.zip
+    python3 -m pip install -r requirements.txt
+    cd venv/lib/python3.8/site-packages
+    zip -r9 ${OLDPWD}/app.zip . -x "numpy*" "scipy*"
+    cd $OLDPWD
+    zip -rg app.zip . -x ".git*" "node_modules/*" "venv/*" "*.DS_Store"\
+        ".vscode/*" "deploy.sh" "app/dist/*" "app/static/*" "*__pycache__/*"\
+        "package.json" "package-lock.json" "webpack*" ".babelrc" ".env"\
+        ".eslintrc.js" "README.md"
+    aws s3 cp app.zip s3://fantasy-sports-analytics
+    rm app.zip
+    aws lambda update-function-code --function-name fantasy-sports-analytics\
+        --s3-bucket fantasy-sports-analytics\
+        --s3-key app.zip
 fi
